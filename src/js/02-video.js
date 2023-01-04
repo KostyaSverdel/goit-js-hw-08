f2749ed6d9fe6a5aa548272398b7dbb460ee4b9d
KostyaSverdel <109824269+KostyaSverdel@users.noreply.github.com>
2023-01-04 13:20:39 +0200

    task-2

---
+++ src/js/02-video.js
@@ -0,0 +1,25 @@
+import Player from '@vimeo/player';
+const _throttle = require('lodash.throttle');
+
+const iframe = document.querySelector('iframe');
+const iframePlayer = new Player(iframe);
+
+localStorage.getItem('videoplayer-current-time')
+  ? iframePlayer.setCurrentTime(
+      localStorage.getItem('videoplayer-current-time')
+    )
+  : 0;
+
+iframePlayer.on(
+  'timeupdate',
+  _throttle(function (e) {
+    localStorage.setItem(
+      'videoplayer-current-time',
+      Math.floor(e.seconds).toString()
+    );
+    console.log(
+      'videoplayer-current-time (seconds) >> ',
+      localStorage.getItem('videoplayer-current-time')
+    );
+  }, 1000)
+);
